421ee6b39994cedd98b8cfec9a31aec42ed1bfc8
DanSikQ <sekinaev1234@mail.ru>
2025-10-23 18:40:59 +0300

    1

--- SQL/Запросы для дашборда/Продажи за неделю.sql
+++
@@ -1,4 +0,0 @@
-select fs.*, SUM(revenue) OVER(Order by sale_date) acc_revenue, SUM(margin) OVER(Order by sale_date) as acc_margin from fact_sales fs
-left join dim_clients dc on fs.client_id = dc.client_id
-Where sale_date between '2023-11-01' and '2023-11-30' and dc.manager_id = 2
-order by sale_date
